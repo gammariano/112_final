7800fff318f26731471330764d2b6544967f0a9f
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-29 18:41:05 +0700

    No Magic Square

--- 112_final_3.c
+++ 112_final_3.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n1,n2,n3,i,j;
+    int n1,n2,n3,i,j,rows,columns,diagonals,row1,row2,row3,column1,column2,column3,diagonal1,diagonal2,diagonal3;
     scanf("%d %d %d" ,&n1,&n2,&n3);
     int s1[10][10],s2[10][10],s3[10][10];
     printf("\n");
@@ -27,6 +27,44 @@ int main()
         {
             scanf("%d" ,&s3[i][j]);
         }
+    }
+    for(i=0;i<n1;i++)
+    {
+        for(j=0;j<n1;j++)
+        {
+            row1+=s1[i][0];
+            row2+=s1[i][1];
+            row3+=s1[i][2];
+        }
+    }
+    rows=row1+row2+row3;
+    for(i=0;i<n1;i++)
+    {
+        for(j=0;j<n1;j++)
+        {
+            column1+=s1[0][j];
+            column2+=s1[1][j];
+            column3+=s1[2][j];
+        }
+    }
+    columns=column1+column2+column3;
+    for(i=0;i<n1;i++)
+    {
+        for(j=0;j<n1;j++)
+        {
+            if(s1[i][j]==s1[j][i])
+            {
+                diagonal1+=s1[i][j];
+            }
+        }
+    }
+    if(rows==columns)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
     }
 
     return 0;
