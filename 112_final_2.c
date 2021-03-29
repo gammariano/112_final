872ebac939d96806a9873c71d44b7bf5d8792a4a
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-29 17:27:02 +0700

    ลูบไล้ (looblike)

--- 112_final_2.c
+++ 112_final_2.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,l;
+    int n,i,j,sum,max=0;
     scanf("%d" ,&n);
     int looblike[n];
     for(i=0;i<n;i++)
     {
         scanf("%d" ,&looblike[i]);
     }
+    for(i=0;i<n;i++)
+    {
+        if(looblike[i]==looblike[i+1])
+        {
+            max=looblike[i];
+        }
+    }
+    printf("%d" ,max);
 
     return 0;
 }
