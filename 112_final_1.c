278ece4a236a2f79fc5b20591fbdc144e5618002
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-29 17:47:21 +0700

    Nearest Number

--- 112_final_1.c
+++ 112_final_1.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,k,c,i,j;
+    int n,k,c,i,j,num1,num2;
     scanf("%d %d" ,&n,&c);
+    num1=n;
+    n=n/10;
+    if(n<num1)
+    {
+        n+=1;
+        printf("%d" ,n);
+    }
+    else
+    {
+        printf("%d" ,n);
+    }
+    printf("%d" ,c);
 
     return 0;
 }
