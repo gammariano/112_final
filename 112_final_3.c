a634c6a569b7c89e2bad94727231aaa2ad8a8651
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-29 18:13:34 +0700

    No Magic Square

--- 112_final_3.c
+++ 112_final_3.c
@@ -1,8 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int n1,n2,n3;
+    int n1,n2,n3,i,j;
     scanf("%d %d %d" ,&n1,&n2,&n3);
+    int s1[10][10],s2[10][10],s3[10][10];
+    printf("\n");
+    for(i=0;i<n1;i++)
+    {
+        for(j=0;j<n1;j++)
+        {
+            scanf("%d" ,&s1[i][j]);
+        }
+    }
+    printf("\n");
+    for(i=0;i<n2;i++)
+    {
+        for(j=0;j<n2;j++)
+        {
+            scanf("%d" ,&s2[i][j]);
+        }
+    }
 
     return 0;
 }
