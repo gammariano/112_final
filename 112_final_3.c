ca420f48d0f9d3446835c123ded0812cf6dfcce5
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-03-29 18:56:52 +0700

    No Magic Square

--- 112_final_3.c
+++ 112_final_3.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n1,n2,n3,i,j,rows,columns,diagonals,row1,row2,row3,column1,column2,column3,diagonal1,diagonal2,diagonal3;
+    int n1,n2,n3,i,j,row1,row2,row3,row4,row5,column1,column2,column3,column4,column5,diagonal1,diagonal2,diagonal3,diagonal4,diagonal5;
+    int rows1,rows2,rows3,columns1,columns2,columns3,diagonals1,diagonals2,diagonals3;
     scanf("%d %d %d" ,&n1,&n2,&n3);
     int s1[10][10],s2[10][10],s3[10][10];
     printf("\n");
@@ -13,7 +14,7 @@ int main()
         }
     }
     printf("\n");
-    for(i=0;i<n2;i++)
+    /*for(i=0;i<n2;i++)
     {
         for(j=0;j<n2;j++)
         {
@@ -28,37 +29,26 @@ int main()
             scanf("%d" ,&s3[i][j]);
         }
     }
-    for(i=0;i<n1;i++)
+    */for(i=0;i<n1;i++)
     {
         for(j=0;j<n1;j++)
         {
-            row1+=s1[i][0];
-            row2+=s1[i][1];
-            row3+=s1[i][2];
-        }
-    }
-    rows=row1+row2+row3;
-    for(i=0;i<n1;i++)
-    {
-        for(j=0;j<n1;j++)
-        {
-            column1+=s1[0][j];
-            column2+=s1[1][j];
-            column3+=s1[2][j];
-        }
-    }
-    columns=column1+column2+column3;
-    for(i=0;i<n1;i++)
-    {
-        for(j=0;j<n1;j++)
-        {
+            row1+=s1[i][j];
+            column1+=s1[j][i];
             if(s1[i][j]==s1[j][i])
             {
                 diagonal1+=s1[i][j];
             }
         }
     }
-    if(rows==columns)
+    for(i=3;i>0;i--)
+    {
+        for(j=0;j<n1;j++)
+        {
+                diagonal1+=s1[i][j];
+        }
+    }
+    if(row1==column1&&row1==diagonal1&&column1==diagonal1)
     {
         printf("yes");
     }
